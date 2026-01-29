15d9f7920a738b289383326c57da794cf3f0c64c
breadbread1984 <breadbread1984@163.com>
2026-01-29 14:23:21 +0800

    update

--- run_mcp.py
+++ run_mcp.py
@@ -73,7 +73,7 @@ def parse_args() -> argparse.Namespace:
         "--connection",
         "-c",
         choices=["stdio", "sse"],
-        default="stdio",
+        default="sse",
         help="Connection type: stdio or sse",
     )
     parser.add_argument(
